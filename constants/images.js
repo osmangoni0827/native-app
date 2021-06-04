f7296e1f6d3807d9c5f39535715ab46a9b57f11a
TKRchamak <tkrchamak311298@gmail.com>
2021-06-04 20:14:24 +0600

    manage data

---
+++ constants/images.js
@@ -0,0 +1,36 @@
+// export const nikeMetcon3 = require("../assets/images/nike-metcon-3.png");
+// export const nikeMetcon4 = require("../assets/images/nike-metcon-4.png");
+// export const nikeMetcon5Black = require("../assets/images/nike-metcon-5-black.png");
+// export const nikeMetcon5Purple = require("../assets/images/nike-metcon-5-purple.png");
+// export const nikeMetcon6 = require("../assets/images/nike-metcon-6.png");
+// export const nikeMetconFree = require("../assets/images/nike-metcon-free.png");
+// export const nikePegasus36 = require("../assets/images/nike-pegasus-36.png");
+// export const nikeZoomKobe1Proto = require("../assets/images/nike-zoom-kobe-1-protro.png");
+export const recentlyViewedLabel = require("../assets/images/recently-viewed-label.png");
+export const bagpack1 = require("../assets/img/1282271-14-revolution-checkpoint-friendly-backpack-teb012us-backpacks-laptop-bags-targus-backpack-png-1200_1200 1.png");
+export const bagpack2 = require("../assets/img/image 16.png");
+export const bagpack3 = require("../assets/img/image 4.png");
+export const bagpack4 = require("../assets/img/kisspng-backpacking-travel-hiking-osprey-5b566542cd5807 1.png");
+export const bagpack5 = require("../assets/img/pngwing 1.png");
+export const bagpack6 = require("../assets/img/pngwing 2.png");
+export const bagpack7 = require("../assets/img/pngwing 3.png");
+
+
+export default {
+    // nikeMetcon3,
+    // nikeMetcon4,
+    // nikeMetcon5Black,
+    // nikeMetcon5Purple,
+    // nikeMetcon6,
+    // nikeMetconFree,
+    // nikePegasus36,
+    // nikeZoomKobe1Proto,
+    recentlyViewedLabel,
+    bagpack1,
+    bagpack2,
+    bagpack3,
+    bagpack4,
+    bagpack5,
+    bagpack6,
+    bagpack7
+}
